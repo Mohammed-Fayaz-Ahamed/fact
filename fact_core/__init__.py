03de4b479a7f00cc7b0148b6d3958598d37fbf35
Mohammed-Fayaz-Ahamed <mohammedfayazahamed20@gmail.com>
2026-06-28 19:37:01 +0530

    feat: expose package version

--- fact_core/__init__.py
+++ fact_core/__init__.py
@@ -1,7 +1,15 @@
+from importlib.metadata import PackageNotFoundError, version
+
 from .config import FactConfig
 from .metrics import metrics
+
+try:
+    __version__ = version("fact-telemetry")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
 
 __all__ = [
     "FactConfig",
     "metrics",
+    "__version__",
 ]
